b4754ca7bcf58c7f733fa0da5ec17c495d02f6ea
yumichael <michael.yang2357@gmail.com>
2016-09-29 18:54:50 -0400

    testing with files provided by workshop

---
+++ script.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+
+
+  $("#portfolioLoad").on("click",function(){
+    $("#jumbotron").addClass("hidden");
+    $("#contactme").addClass("hidden");
+    $("#portfolio").removeClass("hidden");
+    $("#about").addClass("hidden");
+    $("#resume").addClass("hidden");
+    $("#portfolioli").addClass("active");
+    $("#aboutli").removeClass("active");
+    $("#contactli").removeClass("active");
+    $("#resumeli").removeClass("active");
+    $('#portfolio').fadeIn(1);
+  });
+  
+  $("#resumeLoad").on("click",function(){
+    $("#jumbotron").addClass("hidden");
+    $("#contactme").addClass("hidden");
+    $("#portfolio").addClass("hidden");
+    $("#about").addClass("hidden");
+    $("#resume").removeClass("hidden");
+    $("#portfolioli").removeClass("active");
+    $("#resumeli").addClass("active");
+    $("#contactli").removeClass("active");
+    $("#aboutli").removeClass("active");
+  });
+
+  $("#aboutLoad").on("click",function(){
+    $("#jumbotron").removeClass("hidden");
+    $("#contactme").addClass("hidden");
+    $("#portfolio").addClass("hidden");
+    $("#about").removeClass("hidden");
+    $("#resume").addClass("hidden");
+    $("#portfolioli").removeClass("active");
+    $("#aboutli").addClass("active");
+    $("#contactli").removeClass("active");
+    $("#resumeli").removeClass("active");
+  });
+
+  $("#contactLoad").on("click",function(){
+    $("#jumbotron").addClass("hidden");
+    $("#contactme").removeClass("hidden");
+    $("#portfolio").addClass("hidden");
+    $("#about").addClass("hidden");
+    $("#resume").addClass("hidden");
+    $("#portfolioli").removeClass("active");
+    $("#aboutli").removeClass("active");
+    $("#contactli").addClass("active");
+    $("#resumeli").removeClass("active");
+    
+  });
+
+
+
+});
